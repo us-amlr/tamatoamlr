b9ea28aa2d72318caf0f5f1cf8282f9cf6024c90
Sam Woodman <sam.woodman@noaa.gov>
2021-03-23 16:43:02 -0700

    renamed to amlr_pinnipeds_gui, first draft of pinniped_age function

---
+++ R/pinniped_age.R
@@ -0,0 +1,42 @@
+#' Pinniped age calculation
+#'
+#' Calculate the age of a pinniped based on a given date and cohort year
+#'
+#' @param event.date object of class \code{Date}; the date at which you wish to determine the pinniped's age
+#' @param cohort object interpretable as number(s),
+#'   meaning \code{\link[base:numeric]{is.numeric}(cohort)} must be \code{TRUE}.
+#'   The cohort year of the pinniped.
+#'
+#' @details \code{event.date} (event date) and \code{cohort} must be the same length.
+#'
+#'   The age of the pinniped is calculated by taking the year of the event date,
+#'   subtracting the cohort year, and subtracting an additional 1 if the month of
+#'   the event date is less than 10 (before October).
+#'   For instance, if the event date is 15 Nov 2010 and the cohort is 2005, the pinniped age is 2010 - 2005 - 0 = 5
+#'   If the event date is 15 Feb 2010 and the cohort is 2005, then the pinniped age is 2010 - 2005 - 1 = 4.
+#'
+#'   If either the event date or the cohort is \code{NA}, then the pinniped age will be \code{NA}
+#'
+#' @return A numeric vector of the same length as the input vectors
+#'
+#' @examples
+#' x <- data.frame(
+#'   resight_date = as.Date(c("2012-11-01", "2012-03-01", "2014-02-01")),
+#'   pinniped_id = c(1, 1, 2),
+#'   cohort = c(2010, 2010, 2011)
+#' )
+#'
+#' pinniped_age(x$resight_date, x$cohort)
+#'
+#' @export
+pinniped_age <- function(event.date, cohort) {
+  stopifnot(
+    length(event.date) == length(cohort),
+    inherits(event.date, "Date"),
+    is.numeric(cohort)
+  )
+
+  as.numeric( #in case of all NAs
+    lubridate::year(event.date) - cohort - ifelse(lubridate::month(event.date) < 10, 1, 0)
+  )
+}
---
+++ data-raw/testing.R
@@ -0,0 +1,30 @@
+## This script is a place to connect to/pull data from db for direct testing
+
+library(DBI)
+library(odbc)
+
+library(amlrPinnipeds)
+library(dplyr)
+# library(lubridate)
+# library(readxl)
+
+tableNA <- function(...) table(..., useNA = 'ifany')
+
+con <- dbConnect(odbc(),
+                 Driver = "SQL Server", #"SQL Server Native Client 11.0",
+                 Server = "swc-estrella-s",
+                 Database = "AMLR_PINNIPEDS_Test",
+                 Trusted_Connection = "True")
+
+# pinniped.season <- tbl(con, "pinniped_season") %>% collect()
+
+tr.p <- tbl(con, "tag_resights") %>%
+  left_join(tbl(con, "pinnipeds"), by = c("pinniped_id" = "ID")) %>%
+  filter(!is.na(resight_date)) %>%
+  head(5000) %>%
+  select(-ts, -attachments) %>%
+  collect() %>%
+  mutate(resight_date = as.Date(resight_date),
+         pinniped_age = pinniped_age(resight_date, cohort)) %>%
+  select(pinniped_age, cohort, everything())
+
