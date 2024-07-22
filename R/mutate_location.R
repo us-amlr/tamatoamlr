adf26a605c99658927488d074b445ca597587833
Sam Woodman - NOAA <sam.woodman@noaa.gov>
2024-07-22 16:43:13 -0700

    Update locations

--- R/mutate_location.R
+++ R/mutate_location.R
@@ -51,7 +51,7 @@ mutate_location <- function(x) {
                loc_lower %in% tolower(c("Ballena hill", "Ballena hills", "Ballena H")) ~ "Ballena Hill",
                loc_lower %in% tolower(c("Ballena", "Ballena N", "Ballena THrte", "B Nor", "Ballena Norte", "BNorte/Sur", "B Norte", "BNorte", "Plan Ball Norte", "Ballena C", "BN")) ~ "Ballena Norte",
                loc_lower %in% tolower(c("Ballena flats", "Ball Fl", "Ballena Fl", "B Norte flats", "BNorte Flats", "Ballena N Flats", "Ballena Centra Flats", "Flats Ballenas", "Flats Ballena Norte", "Planicie Ballena norte", "Ballena F", "Ballena W", "Ballena")) ~ "Ballena Flats",
-               loc_lower %in% tolower(c("Ballena S", "Ball S", "BSur", "BSur ridge", "B Sur", "ballena sur", "Pla BallSur", "Planicie Ballena Sur")) ~ "Ballena Sur",
+               loc_lower %in% tolower(c("Ballena S", "Ballend S", "Ball S", "BSur", "BSur ridge", "B Sur", "ballena sur", "Pla BallSur", "Planicie Ballena Sur", "Ballend/Chag")) ~ "Ballena Sur",
                loc_lower %in% tolower(c("Plan Ball Sur", "B Sur flats", "Plan Ballena Sur", "BSur flats")) ~ "Ballena Sur Flats",
 
                loc_lower %in% tolower(c("Cach", "Cach slope", "Cacheros", "Cacharros", "Cach/Chung", "Cachomos", "Mad/Cach")) ~ "Cachorros",
@@ -63,13 +63,13 @@ mutate_location <- function(x) {
                loc_lower %in% tolower(c("Cach H", "Cach Hill")) ~ "Cachorros Hill",
 
                loc_lower %in% tolower(c("Chugungo", "Chango", "Chung high", "Chung", "Chun", "Chungungu", "Chungunga", "Chung/Ballena")) ~ "Chungungo",
-               loc_lower %in% tolower(c("E Chungungo", "Chung E high", "Chung (E)", "Chung(E)", "Chung-E", "Chung E", "Chungungo east", "Chin E")) ~ "Chungungo East",
+               loc_lower %in% tolower(c("E Chungungo", "Chung E high", "Chung (E)", "Chung(E)", "Chung-E", "Chung E", "Chungungo east", "Chin E", "Chagunc? Chag E?")) ~ "Chungungo East",
                loc_lower %in% tolower(c("Chung-C", "Chu C", " Chung C/W", "Chung C", "Chungungo C", "Chun C")) ~ "Chungungo Central",
                loc_lower %in% tolower(c("Chungungo W", "Chung W", "Chung-W")) ~ "Chungungo West",
                loc_lower %in% tolower(c("Chung Flats", "Chung Flat", "Chun Flats", "Chung Fl", "Chung flat C", "Plan Chungungo", "Chung F", "Chung F C", "Chung C flat", "Chungunga Flat", "Chung Fts C", "chungungo flats", "chung-flat", "Flats Chungungo", "Flats Chungungo", "Planicie Chungungo", "Chung flats C", "Ch Flats")) ~ "Chungungo Flats",
                loc_lower %in% tolower(c("Chung Flats E", "Chu F E", "Chung F E", "Chung E Flats", "Chung flat E", "Chung E flat")) ~ "Chungungo East Flats",
                loc_lower %in% tolower(c("Chung Flats W", "Chung flat W", "Chung f W", "Chung W flat")) ~ "Chungungo West Flats",
-               loc_lower %in% tolower(c("Chung R", "Chung W ridge", "Chung ridge")) ~ "Chungungo Ridge",
+               loc_lower %in% tolower(c("Chung R", "Chung W ridge", "Chung ridge", )) ~ "Chungungo Ridge",
                loc_lower %in% tolower(c("Chung-hill", "Chung W hill", "Chung F C hill", "Chung F hill", "Chung cliff", "Cerro Chungungo", "Chung hill", "Chungungo hill", "Cerro Chung", "Co Chungungo", "Chung H")) ~ "Chungungo Hill",
 
                loc_lower %in% tolower(c("CopiHue High", "Copi Hue")) ~ "Copihue",
@@ -77,7 +77,7 @@ mutate_location <- function(x) {
                loc_lower %in% tolower(c("Copi-E", "Copi E")) ~ "Copi East",
                loc_lower %in% tolower(c("Copi-C", "Copi C", "Copi C/E", "Copi C high", "Copi-C/W")) ~ "Copi Central",
                loc_lower %in% tolower(c("Cop-W", "Copi-W", "Copi W")) ~ "Copi West",
-               loc_lower %in% tolower(c("Cop Flats", "Copi flats", "Copi Fl", "Copi F", "Copi-flats", "Copihue slope", "Plan Copihue", "Plan Vero", "Planicie Copihue", "Flats Copihue", "Flats Copihue", "Plan Copi", "Cop F", "Copihue Flats", "Copihue plano")) ~ "Copi Flats",
+               loc_lower %in% tolower(c("Cop Flats", "Copi flats", "Copi Fl", "Copi F", "Copi Flat", "Copi-flats", "Copihue slope", "Plan Copihue", "Plan Vero", "Planicie Copihue", "Flats Copihue", "Flats Copihue", "Plan Copi", "Cop F", "Copihue Flats", "Copihue plano")) ~ "Copi Flats",
                loc_lower %in% tolower(c("Copi Flats E", "Copi E Flats")) ~ "Copi Flats East",
                loc_lower %in% tolower(c("Copi Flats W", "Copi W Flats")) ~ "Copi Flats West",
                loc_lower %in% tolower(c("Copi hill", "copi-hill", "Copi H", "Hue H")) ~ "Copi Hill",
@@ -87,7 +87,7 @@ mutate_location <- function(x) {
 
                loc_lower %in% tolower(c("Hue", "Hue wE")) ~ "Hue",
                loc_lower %in% tolower(c("Hue-E", "Hue E")) ~ "Hue East",
-               loc_lower %in% tolower(c("Hue-C", "Hue C", "Hue C high", "Hue C/W")) ~ "Hue Central",
+               loc_lower %in% tolower(c("Hue-C", "Hue C", "Hue C high", "Hue C/W", "Huey C")) ~ "Hue Central",
                loc_lower %in% tolower(c("Hue-W", "Hue W")) ~ "Hue West",
                loc_lower %in% tolower(c("Hue Far E")) ~ "Hue Far East",
                loc_lower %in% tolower(c("Hue S")) ~ "Hue South",
@@ -111,12 +111,12 @@ mutate_location <- function(x) {
                loc_lower %in% tolower(c("Lob flats", "Loberia F", "Loberia flats", "Loberia flats S", "lobe ria valley", "Loberia Fl")) ~ "Loberia Flats",
                loc_lower %in% tolower(c("Lobe ria H", "Lob H")) ~ "Loberia Hill",
 
-               loc_lower %in% tolower(c("Mad", "Mad S", "Mad N", "Mad high", "Mad (M)", "Mad-A", "Mad slope")) ~ "Maderas",
+               loc_lower %in% tolower(c("Mad", "Mad S", "Mad N", "Mad high", "Mad (M)", "Mad-A", "Mad slope", "Md Der?")) ~ "Maderas",
                loc_lower %in% tolower(c("Mad (E)", "Mad(E)", "Mad-E", "Mad E high", "Mad E", "Maderas E")) ~ "Maderas East",
                loc_lower %in% tolower(c("Mad (C)", "Mad(C)", "Mad-C", "Mad C", "Mad C/W")) ~ "Maderas Central",
                loc_lower %in% tolower(c("Mad (W)", "Mad-w", "Mad w", "Mad(W)")) ~ "Maderas West",
                loc_lower %in% tolower(c("Mad Flats", "Mad F", "Mad flat", "Mad-F", "mad-flats", "above Maderas", "Maderas flat", "Maderas High")) ~ "Maderas Flats",
-               loc_lower %in% tolower(c("Mad-r", "Mad R", "Mad Ridge", "Maderas Rdg")) ~ "Maderas Ridge",
+               loc_lower %in% tolower(c("Mad-r", "Mad R", "Mad Ridge", "Maderas Rdg", "HD. R.?", "Mad B")) ~ "Maderas Ridge",
                loc_lower %in% tolower(c("Mad Saddle")) ~ "Maderas Saddle",
 
                # loc_lower %in% tolower(c("Módulo", "PMod", "Modelo", "landing beach", "Mod", "modulo", "Pl El Modulo", "El Módulo", "Playa Modulo", "Above int'l highway", "International highway", "PMod", "Highway",
@@ -128,14 +128,14 @@ mutate_location <- function(x) {
                loc_lower %in% tolower(c("P Modulo S")) ~ "Modulo South",
                loc_lower %in% tolower(c("Lago Daniel", "Mod S", "Modulo S", "Mod South", "Mods")) ~ "Modulo South",
                loc_lower %in% tolower(c("Flats Modulo", "Modulo heights", "Mod Flats", "modulo flats", "Modulo Road", "El Condor/Modulo", "Modulo Flat",
-                                        "modulo-camp", "Modulo/Camp", "Mod F")) ~ "Modulo Flats",
+                                        "modulo-camp", "Modulo/Camp", "Mod Fl", "Mod F")) ~ "Modulo Flats",
 
                loc_lower %in% tolower(c("Cabo Lodge", "Campamento", "Flats Daniel", "Daniel/Flats", "Daniel/Flats", "Chilean Camp", "Flat near camp", "camp", "Flats, Near Camp")) ~ "Camp",
-               loc_lower %in% tolower(c("Playa Daniel", "daniel", "Danile", "Danilel", "Daniel H", "Plan Daniel", "P Daniel", "P Daniel high", "PDaniel", "P Daniel", "Planicie Daniel", "Dorsal Daniel", "Pl Daniel", "Daiel")) ~ "Daniel",
-               loc_lower %in% tolower(c("Playa Marko", "Marco", "Marko hill", "Marko H", "Plan Marko", "marko ridge", "PMarko", "P Marka", "P Marko", "marko", "Marko C", "Marko N", "Playa Marka", "marko/condor slope")) ~ "Marko",
+               loc_lower %in% tolower(c("Playa Daniel", "daniel", "Danile", "Danilel", "Daniel H", "Plan Daniel", "P Daniel", "P Daniel high", "PDaniel", "P Daniel", "Planicie Daniel", "Dorsal Daniel", "Pl Daniel", "Daiel", "Daniel F")) ~ "Daniel",
+               loc_lower %in% tolower(c("Playa Marko", "Marco", "Marko hill", "Marko H", "Plan Marko", "marko ridge", "PMarko", "P Marka", "P Marko", "marko", "Marko C", "Marko N", "Playa Marka", "marko/condor slope", "Maduo?")) ~ "Marko",
                loc_lower %in% tolower(c("El Condor Flats", "marko flats", "Flats Larga", "Larga flats", "Flats Marko")) ~ "Marko Flats",
                loc_lower %in% tolower(c("S Playa Largo", "Playa Larga High", "playa larga", "P Larga", "PLarga")) ~ "Larga",
-               loc_lower %in% tolower(c("P Media Luna", "media luna", "Media Luna flats", "Medi Luna", "Meda Luna", "Meda Luna S", "Media L", "S Pl Media Luna", "M Luna", "MLuna", "MML", "ML", "towards Media Luna")) ~ "Media Luna",
+               loc_lower %in% tolower(c("P Media Luna", "media luna", "Media Luna flats", "Medi Luna", "Meda Luna", "Meda Luna S", "Media L", "S Pl Media Luna", "M Luna", "MLuna", "MML", "ML", "towards Media Luna", "Mu Lu?")) ~ "Media Luna",
 
                loc_lower %in% tolower(c("Ivaldo", "Pl N Baldo", "Nivaldo", "Nibaldo Bahamondes", "P Nibaldo")) ~ "Nibaldo",
                loc_lower %in% tolower(c("PNegra", "PNegra east", "P Negra")) ~ "Punta Negra",
@@ -157,7 +157,7 @@ mutate_location <- function(x) {
                loc_lower %in% tolower(c("Valley Corto")) ~ "Valle Corto",
                # loc_lower %in% tolower(c("P Yamana", "Yámana", "Yamana", "Yamana Flats", "Pl Yamana", "Playa Yamana")) ~ "Yamana",
                loc_lower %in% tolower(c("P Yamana", paste0("Y", "\\u00e1", "mana"), "Yamana", "Yamana Flats", "Pl Yamana", "Playa Yamana")) ~ "Yamana",
-               loc_lower %in% tolower(c("P Yuseff", "ML/P Yusseff", "P Yusseff", "Punta Yuseff")) ~ "Punta Yuseff",
+               loc_lower %in% tolower(c("P Yuseff", "ML/P Yusseff", "P Yusseff", "Punta Yuseff", "Punta J")) ~ "Punta Yuseff",
 
                # "P Golondrina to P del Lobero", "P Delphin to P La Caverna", "P Paulina to P Aranda"
                # loc_lower %in% tolower(c("WST")) ~ "Unknown",
