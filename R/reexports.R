29550aef44adc7b2bc0f01848d31ffffea572eda
Sam Woodman - NOAA <sam.woodman@noaa.gov>
2025-05-06 13:37:35 -0700

    Added reexport

--- R/reexports.R
+++ R/reexports.R
@@ -10,6 +10,10 @@ amlrDatabases::amlr_date_from_season
 #' @export
 amlrDatabases::amlr_season_from_date
 
+#' @importFrom amlrDatabases amlr_year_from_season
+#' @export
+amlrDatabases::amlr_year_from_season
+
 #' @importFrom amlrDatabases round_logical
 #' @export
 amlrDatabases::round_logical
